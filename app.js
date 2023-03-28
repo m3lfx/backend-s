3f5eb0ed40cfb49efea48d48ad6d39e330a87602
unknown <rommelmdalisay@gmail.com>
2023-03-28 09:49:25 +0800

    cors origin

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser')
-
+const cors = require('cors')
 const products = require('./routes/product');
 const auth = require('./routes/auth');
 const order = require('./routes/order');
 const errorMiddleware = require('./middlewares/errors');
 
 app.use(express.json({ limit: '100mb' }));
+app.use(cors({
+    origin: "https://frontend-s.onrender.com",
+    credentials: true}))
 app.use(cookieParser());
 app.use(express.urlencoded({limit: "100mb", extended: true }));
 
