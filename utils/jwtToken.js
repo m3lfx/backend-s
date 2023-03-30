9607fae3adf07a495aee6178e2e18a6331adf9b2
unknown <rommelmdalisay@gmail.com>
2023-03-30 10:14:21 +0800

    cookie samesite

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -8,7 +8,9 @@ const sendToken = (user, statusCode, res) => {
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
         ),
-        httpOnly: true
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none'
     }
     res.status(statusCode).cookie('token', token, options).json({
         success: true,
